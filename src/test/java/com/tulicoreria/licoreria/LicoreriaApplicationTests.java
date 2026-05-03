937240a07abab03308eb5abeb1d98e9e12f65150
sergio <sergiotafbet@gmail.com>
2026-05-03 09:39:48 -0500

    primer comit

---
+++ src/test/java/com/tulicoreria/licoreria/LicoreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulicoreria.licoreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicoreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
